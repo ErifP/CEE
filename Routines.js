93df42dbd072d14e246d1e7dbbb691bf287d2c38
ErifP <56601074+ErifP@users.noreply.github.com>
2019-11-17 16:51:50 +0000

    Erif's weekend work 17/11/19
    HTML for routines and relation, plus js for sleep calculaor

---
+++ Routines.js
@@ -0,0 +1,18 @@
+console.log('We are going to do some JS for routines')
+
+function calculateBedTime() {
+    console.log('We are going to tell you when to go to bed')
+    var wakeUpHourElement = document.getElementById('wakeUpHour');
+    var sleepForElement = document.getElementById('sleepFor');
+
+    var wakeUpHour = wakeUpHourElement.value;
+    var sleepFor = sleepForElement.value;
+
+    var bedTime = wakeUpHour - sleepFor;
+    if (bedTime < 0) {
+        bedTime = bedTime + 24;
+    }
+
+    var bedTimeElement = document. getElementById('bedTime');
+    bedTimeElement.innerHTML = "Bedtime is: " + bedTime + ":00";
+}
